13b0baded7b538ab95e874c531aefcc73ed28bff
sung-mac <sungwonchung3@gmail.com>
2019-07-01 04:55:54 +0000

    cron scheduling and visualize state

--- examples/etl.py
+++ examples/etl.py
@@ -1,10 +1,11 @@
 #%%
 from prefect import Flow, task
+from prefect.schedules import CronSchedule
 
 
 @task
 def extract():
-    return [1, 2, 3]
+    return [1, 2, 3, 50]
 
 
 @task
@@ -21,5 +22,10 @@ with Flow("ETL") as flow:
     e = extract()
     t = transform(e)
     l = load(t)
+    schedule = CronSchedule("0 0 * * *")  # setup a cron scheduler
 
-flow.run()
+flow_state = flow.run()  # set the flow run to an object to track state
+
+flow.visualize(flow_state=flow_state)  # visualize how the data moves throughout the DAG
+
+#%%
